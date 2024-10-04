8e31abdfb93db061d77ee3a30ed950c141ee2f07
tathsaraniliyanage <prabodhathathsarani28@gmail.com>
2024-10-04 09:59:55 +0530

    note controller updated

--- src/main/java/lk/ijse/gdse/notetaker/controller/NoteController.java
+++ src/main/java/lk/ijse/gdse/notetaker/controller/NoteController.java
@@ -5,7 +5,9 @@ import lk.ijse.gdse.notetaker.service.NoteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,8 +25,21 @@ public class NoteController {
     @Autowired
     private final NoteService noteService;
 
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> createNote(@RequestBody NoteDto note){
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        if (note == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }else {
+            try {
+                noteService.saveNote(note);
+                return new ResponseEntity<>(HttpStatus.CREATED);
+            }catch (DataPersistFailedException e){
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }catch (Exception e){
+                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
     }
+
 
 }
