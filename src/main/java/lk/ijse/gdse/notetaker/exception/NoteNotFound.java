8cb4b0363dc0097cf9678ca85ab8cd29815f7019
tathsaraniliyanage <prabodhathathsarani28@gmail.com>
2024-10-04 10:59:51 +0530

    NoteNotFound custom exception added

---
+++ src/main/java/lk/ijse/gdse/notetaker/exception/NoteNotFound.java
@@ -0,0 +1,11 @@
+package lk.ijse.gdse.notetaker.exception;
+
+public class NoteNotFound extends RuntimeException {
+    public NoteNotFound(String message) {
+
+    }
+  public NoteNotFound() {
+  }
+  public NoteNotFound(String message, Throwable cause) {
+  }
+}
