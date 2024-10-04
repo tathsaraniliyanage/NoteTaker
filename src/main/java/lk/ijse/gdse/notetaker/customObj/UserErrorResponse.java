966f934cc6b62fb463fa2a44d92ceef39f289c7b
tathsaraniliyanage <prabodhathathsarani28@gmail.com>
2024-10-04 10:50:15 +0530

    custom object for user created

---
+++ src/main/java/lk/ijse/gdse/notetaker/customObj/UserErrorResponse.java
@@ -0,0 +1,11 @@
+package lk.ijse.gdse.notetaker.customObj;
+
+/**
+ * @author Prabodha Thathsarani
+ * @date 10/4/24
+ * @project notetaker
+ **/
+public class UserErrorResponse implements UserResponse{
+    private int errorCode;
+    private String errorMessage;
+}
---
+++ src/main/java/lk/ijse/gdse/notetaker/customObj/UserResponse.java
@@ -0,0 +1,4 @@
+package lk.ijse.gdse.notetaker.customObj;
+
+public interface UserResponse {
+}
