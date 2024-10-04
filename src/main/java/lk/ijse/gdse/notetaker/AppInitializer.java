6b2f78a97b595406742021f8ddc1d52c51173533
tathsaraniliyanage <prabodhathathsarani28@gmail.com>
2024-10-04 09:21:56 +0530

    project updated

--- src/main/java/lk/ijse/gdse/notetaker/AppInitializer.java
+++ src/main/java/lk/ijse/gdse/notetaker/AppInitializer.java
@@ -1,28 +1,6 @@
 package lk.ijse.gdse.notetaker;
 
-import java.io.*;
 
-import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.*;
+public class AppInitializer  {
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
-public class AppInitializer extends HttpServlet {
-    private String message;
-
-    public void init() {
-        message = "Hello World!";
-    }
-
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
-    }
-
-    public void destroy() {
-    }
 }
