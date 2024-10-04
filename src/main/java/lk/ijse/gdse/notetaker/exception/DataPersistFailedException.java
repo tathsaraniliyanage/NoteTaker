00f553038c9c146de7aee947e3c21ce371ebeacd
tathsaraniliyanage <prabodhathathsarani28@gmail.com>
2024-10-04 11:03:02 +0530

    DataPersistFailed custom exception added

---
+++ src/main/java/lk/ijse/gdse/notetaker/exception/DataPersistFailedException.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse.notetaker.exception;
+
+public class DataPersistFailedException extends RuntimeException {
+    public DataPersistFailedException(String message) {
+        super(message);
+    }
+}
