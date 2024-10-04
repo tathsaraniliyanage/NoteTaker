2409964f569a7d2c9a2952d367660f7d3884a312
tathsaraniliyanage <prabodhathathsarani28@gmail.com>
2024-10-04 11:04:13 +0530

    DataPersistFailed custom exception updated

--- src/main/java/lk/ijse/gdse/notetaker/exception/DataPersistFailedException.java
+++ src/main/java/lk/ijse/gdse/notetaker/exception/DataPersistFailedException.java
@@ -1,7 +1,13 @@
 package lk.ijse.gdse.notetaker.exception;
 
 public class DataPersistFailedException extends RuntimeException {
-    public DataPersistFailedException(String message) {
-        super(message);
+    public DataPersistFailedException() {
     }
+
+    public DataPersistFailedException(String message) {
+    }
+
+    public DataPersistFailedException(String message, Throwable cause) {
+    }
+
 }
